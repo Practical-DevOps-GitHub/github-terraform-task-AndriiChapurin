86fc9e20e3ee81f9a62459341aa2e6f41473a65e
AndriiChapurin <138620764+AndriiChapurin@users.noreply.github.com>
2023-08-06 00:01:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "github_actions_secret" "example_secret" {
   plaintext_value = "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"
 }
 
-
 resource "github_repository" "example" {
   name       = "github-terraform-task-AndriiChapurin"
   auto_init  = true
@@ -23,22 +22,6 @@ resource "github_branch" "develop" {
 resource "github_branch_default" "default" {
   repository = github_repository.example.name
   branch     = github_branch.develop.branch
-}
-
-resource "github_branch_protection" "main" {
-  repository_id  = github_repository.example.node_id
-  pattern        = "main"
-  enforce_admins = true
-
-  required_status_checks {
-    strict   = true
-    contexts = ["ci/test"]
-  }
-
-required_pull_request_reviews {
-    dismiss_stale_reviews          = true
-    require_code_owner_reviews     = true
-  }
 }
 
 resource "github_branch_protection" "develop" {
@@ -58,6 +41,22 @@ resource "github_branch_protection" "develop" {
   }
 }
 
+resource "github_branch_protection" "main" {
+  repository_id  = github_repository.example.node_id
+  pattern        = "main"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = true
+    contexts = ["ci/test"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews          = true
+    require_code_owner_reviews     = true
+  }
+}
+
 resource "github_repository_collaborator" "collaborator" {
   repository = github_repository.example.name
   username   = "softservedata"
@@ -70,6 +69,7 @@ resource "github_repository_file" "codeowners" {
   content    = "* @softservedata"
   branch     = "main"
 }
+
 resource "github_repository_deploy_key" "deploy_key" {
   repository = github_repository.example.name
   key        = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD5Y7Y4SStWqRxgqP0qc5bfWsB3Cji5vRtQMT+ZQNkKfz8YBqZ9IID/9OHCegdGrHAwPVAPuA2jES5pWxQCnPhM7rhKFcguFfPB3ZXDRTDwrhrWY4XfD7Q7g0ObCq6JsxBmNlMwEWOWcTG33LVN8uS96MpC9y8DjP1cYyk6nom+g85XvdX34/YffGav+98AucXybFfrnldigDdyXzLLk8QMrAoREaMbyvlRADr/altWDJ1KdTM4f4JVsyV+ze7zJs8zOCPcEy1gfNB9hU5DmP9Fvpg/OiGozQwDXos0yZ+3dnBzgpdKDnW/njsOloZZIhn7Xtsti7ITbyzKACLWfsaJ Andrii@macs-MacBook-Pro.local"
