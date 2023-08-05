fe4f6a8f07ab84e14e733e08962cfe48f7f90d11
AndriiChapurin <138620764+AndriiChapurin@users.noreply.github.com>
2023-08-05 19:14:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,15 +58,16 @@ resource "github_repository_file" "codeowners" {
 resource "github_repository_file" "pr_template" {
   repository = github_repository.example.name
   file       = ".github/pull_request_template.md"
-  content    = """
+  content    = <<-EOF
   # Describe your changes
-  # Issue ticket number and link
-  # Checklist before requesting a review
+  Issue ticket number and link
+  Checklist before requesting a review
   - [ ] I have performed a self-review of my code
   - [ ] If it is a core feature, I have added thorough tests
   - [ ] Do we need to implement analytics?
   - [ ] Will this be part of a product update? If yes, please write one phrase about this update
-  """
+  EOF
+  branch     = "main"
 }
 resource "github_repository_deploy_key" "deploy_key" {
   repository = github_repository.example.name
